ff06b86d259593b5cabea71b0abe7096aaab4775
telematica <telematica>
2017-03-18 11:25:14 -0500

    Cambio en php

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <head><title>Prueba App Empresariales</title>
 </head>
 <body>
-<?php echo '<p> Hola app empresariales </p>' ?>
+<?php echo '<p> Hola app David Alexander Parra Valencia</p>' ?>
 </body>
 
 </html>
-
